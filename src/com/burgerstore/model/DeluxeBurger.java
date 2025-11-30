9716e95389ef2ec18ec35c8ad2f107882195b527
fishsauce-05 <ManhND.B23AT180@stu.ptit.edu.vn>
2025-11-30 22:00:00 +0700

    java

--- src/com/burgerstore/model/DeluxeBurger.java
+++ src/com/burgerstore/model/DeluxeBurger.java
@@ -2,7 +2,6 @@ package com.burgerstore.model;
 
 public class DeluxeBurger extends Burger {
 
-    // Constructor linh hoạt hơn phiên bản cũ
     public DeluxeBurger(String name, double price) {
         super(name, price);
         this.maxToppings = 5; // Deluxe cho phép 5 topping
@@ -10,7 +9,6 @@ public class DeluxeBurger extends Burger {
 
     @Override
     public double getAdjustedPrice() {
-        // Deluxe Burger không tính thêm tiền topping
         return getBasePrice();
     }
 }
